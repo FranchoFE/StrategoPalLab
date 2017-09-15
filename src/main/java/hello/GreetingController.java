0ec3ef183a17f7296fdf7d2f7749625cd0cecb3e
Francisco Félez Esteban <ffelez@addocean.com>
2017-09-15 10:45:04 +0200

    changed snapSvg to javascript

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -7,10 +7,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
+    private static int radious = 100;
 
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
+        model.addAttribute("radious", radious);
+        System.out.println ("GreetingController.greeting. Radious = " + radious);
+        radious += 10;
         return "greeting";
     }
 
--- src/main/resources/static/js/myScript.js
+++ src/main/resources/static/js/myScript.js
@@ -1,4 +1,10 @@
 
-function myFunction() {
+var s = Snap(850, 600);
+var bigCircle = s.circle(300, 300, 100);
+
+
+function myFunction(param) {
     document.getElementById("demo").style.color = "blue";
+
+    bigCircle.attr("radius", 100);
 }
