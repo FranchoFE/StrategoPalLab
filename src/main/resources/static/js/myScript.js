24faa7416294c30acdeec28efa4cb705b0714788
Francisco Félez Esteban <ffelez@addocean.com>
2017-09-28 18:01:30 +0200

    reading svg values from model

--- src/main/resources/static/js/myScript.js
+++ src/main/resources/static/js/myScript.js
@@ -2,9 +2,24 @@
 var s = Snap(850, 600);
 var bigCircle = s.circle(300, 300, 100);
 
+window.onload = function() {
+  var inputElement = document.getElementById("demo")
+  inputElement.addEventListener('click', function(){
+      gotoNode(document.getElementById("aaa"));
+  });
+};
 
 function myFunction(param) {
     document.getElementById("demo").style.color = "blue";
 
-    bigCircle.attr("radius", 100);
+    bigCircle.attr({fill: "green"});
+    bigCircle.attr({r: 200});
+}
+
+function gotoNode (name)
+{
+    document.getElementById("demo").style.color = "green";
+    var aaa = document.getElementById("aaa").innerHTML;
+    var radiousString = "${radious}";
+    alert (radiousString)
 }
